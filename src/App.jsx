dc56f89d18e83ee570098d1f42193b50c327b362
kumarmanish562 <qmanishkumar562@gmail.com>
2025-08-25 14:07:55 +0530

    feat: add Home component with basic structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
-
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
-      <h1 className="text-4xl font-bold">Hello, Tailwind CSS!</h1>
-    </div>
+    <Routes>
+      <Route  path="/" element={<Home />} />
+    </Routes>
   )
 }
 
