186bd869c90219b6a43583efd4a26e1022d53653
kumarmanish562 <qmanishkumar562@gmail.com>
2025-08-26 00:40:25 +0530

    chore: update package.json scripts and remove gh-pages dependency; refactor main.jsx and vite.config.js for improved structure

--- chatlink/src/main.jsx
+++ chatlink/src/main.jsx
@@ -1,11 +1,13 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/ChatLink">
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom'  
+
+createRoot(document.getElementById('root')).render(
+
+    <BrowserRouter>
       <App />
     </BrowserRouter>
-  </React.StrictMode>
-);
+
+)
--- chatlink/vite.config.js
+++ chatlink/vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/ChatLink/',   // 👈 IMPORTANT for GitHub Pages repo name
-  plugins: [react(), tailwindcss()],
+  plugins: [react(),
+    tailwindcss(),
+  ],
 })
